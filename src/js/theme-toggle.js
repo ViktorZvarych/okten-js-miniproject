007a56e84afc8d62c639aadede6dcb225af1fd03
Viktor Zvarych <viktor.zvarych@gmail.com>
2024-01-12 15:39:41 +0200

    added theme-toggle.js

---
+++ src/js/theme-toggle.js
@@ -0,0 +1,31 @@
+// ------Theme toggle----
+const themeToggle = () => {
+    const htmlElement = document.querySelector('html');
+    const themeToggleInput = document.getElementById('switchThemeInput');
+    const themeToggleLabel = document.getElementById('switchThemeLabel');
+    const theme = localStorage.getItem('theme') || 'dark';
+    if (theme === 'dark') {
+        htmlElement.setAttribute('data-bs-theme', 'dark');
+        themeToggleLabel.innerText = 'Turn ON the lights';
+        themeToggleInput.checked = false;
+    }
+    else {
+        htmlElement.setAttribute('data-bs-theme', 'light');
+        themeToggleLabel.innerText = 'Turn OFF the lights';
+        themeToggleInput.checked = true;
+    }
+    themeToggleInput.onclick = () => {
+        if (themeToggleInput.checked) {
+            localStorage.setItem('theme', 'light');
+            htmlElement.setAttribute('data-bs-theme', 'light');
+        }
+        else {
+            localStorage.setItem('theme', 'dark');
+            htmlElement.setAttribute('data-bs-theme', 'dark');
+        }
+        themeToggleLabel.innerText = !themeToggleInput.checked
+            ? 'Turn ON the lights'
+            : 'Turn OFF the lights';
+    };
+};
+themeToggle();
